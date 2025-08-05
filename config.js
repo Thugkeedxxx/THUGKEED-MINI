cb55e9d634271a2f0c5ef4d34c401b1ca45f0eff
𝗧𝗛𝗨𝗚𝗞𝗘𝗘𝗗 𝗧𝗘𝗖𝗛 <techthugkeed@gmail.com>
2025-08-05 02:00:23 +0200

    Create config.js

---
+++ config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  BOT_NAME: "THUGKEED-XMD",
+  OWNER_NAME: "THUGKEED",
+  OWNER_WHATSAPP: "+2347049474372",
+  CHANNEL_LINK: "https://whatsapp.com/channel/0029VbB7a9v6LwHqDUERef0M",
+  GITHUB_REPO: "https://github.com/thugkeedxxx/THUGKEED-XMD",
+  WELCOME_MESSAGE: `
+┏━━━━━━━━━━━━━━━━━┓
+┃  ⚡ *THUGKEED-XMD* ⚡
+┃  🤖 Powered by THUGKEED TECH
+┗━━━━━━━━━━━━━━━━━┛
+
+🎉 Welcome! Type *!menu* to view commands.
+🔗 Updates: ${this.CHANNEL_LINK}
+  `
+};
