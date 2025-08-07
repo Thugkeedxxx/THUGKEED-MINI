2a41ae57e66674eee0369d062735fcc9e6de033b
𝗧𝗛𝗨𝗚𝗞𝗘𝗘𝗗 𝗧𝗘𝗖𝗛 <techthugkeed@gmail.com>
2025-08-07 23:04:56 +0200

    Update ping.js

--- commands/ping.js
+++ commands/ping.js
@@ -1,3 +1,7 @@
-module.exports = async (sock, msg, reply) => {
-  reply('🏓 Pong! THUGKEED-MINI is online.');
+module.exports = {
+  name: 'ping',
+  description: 'Check if bot is alive',
+  async execute(sock, from) {
+    await sock.sendMessage(from, { text: '🏓 Pong! THUGKEED-LITE-MD is alive.' });
+  }
 };
