04a32d1e8d7a4620520a846664ea7e43272e2984
𝗧𝗛𝗨𝗚𝗞𝗘𝗘𝗗 𝗧𝗘𝗖𝗛 <techthugkeed@gmail.com>
2025-08-08 00:38:52 +0200

    Update config.js

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 
 module.exports = {
   prefix: process.env.PREFIX || '!',
-  pairSite: process.env.PAIR_SITE || 'https://thugkeed-lite-md-pair.onrender.com',
+  pairSite: process.env.PAIR_SITE || 'https://thugkeed-lite-md-pair.onrender.com/pair',
   ownerName: 'THUGKEED',
   ownerContact: 'https://whatsapp.com/channel/0029VbB7a9v6LwHqDUERef0M',
   repo: 'https://github.com/thugkeedxxx/THUGKEED-LITE-MD'
