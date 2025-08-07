b21f17680ecb904707743f2e0947dee2fe8ec558
𝗧𝗛𝗨𝗚𝗞𝗘𝗘𝗗 𝗧𝗘𝗖𝗛 <techthugkeed@gmail.com>
2025-08-07 22:47:14 +0200

    Update config.js

--- config.js
+++ config.js
@@ -1,8 +1,9 @@
+require('dotenv').config();
+
 module.exports = {
+  prefix: process.env.PREFIX || '!',
+  pairSite: process.env.PAIR_SITE || 'https://thugkeed-lite-md-pair.onrender.com',
   ownerName: 'THUGKEED',
-  botName: 'THUGKEED-MINI',
-  ownerNumber: '2347049474372',
-  prefix: '!',
-  channelLink: 'https://whatsapp.com/channel/0029VbB7a9v6LwHqDUERef0M',
-  repo: 'https://github.com/thugkeedxxx/THUGKEED-MINI'
+  ownerContact: 'https://whatsapp.com/channel/0029VbB7a9v6LwHqDUERef0M',
+  repo: 'https://github.com/thugkeedxxx/THUGKEED-LITE-MD'
 };
